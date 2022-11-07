8336a3aaadf58939d1723b94f5eaf9e366df4e22
Gustav Lidberg <gustav98.lidberg@gmail.com>
2022-11-07 17:09:00 +0100

    Created some code for the interface layer

--- interfaces/calibration.py
+++ interfaces/calibration.py
@@ -1,32 +1,50 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
 
 class Calibration(ABC):
     """
-    This is a base class for the relations between a control signal and a target signal of an input or output device.
+    This is a base class for the relations between a target signal and a control signal of an input or output device.
     """
-    def __init__(self, control_unit: str, target_unit: str):
+    def __init__(self, control_unit: str = None, target_unit: str = None) -> None:
         self.control_unit = control_unit # Units for control signal
         self.target_unit = target_unit   # Units for target signal
-    
+
     @abstractmethod
-    def convert(self, control_value: float) -> float:
+    def to_target(self, control_value: float) -> float:
         """
-        Turn control value to target value
+        Turn control value to target value, for example used to read from input devices.
         """
         pass
+
+    @abstractmethod
+    def to_control(self, target_value: float) -> float:
+        """
+        Turn target value to control value, for example used to write to output devices.
+        """
+        pass
+
+    @staticmethod
+    def standard(unit: str = None) -> LinearOutputCalibration:
+        """
+        Factory method for the trivial relation (control=target). Specify only unit of both signals.
+        """
+        return LinearOutputCalibration(1, unit, unit)
 
 class LinearCalibration(Calibration):
     """
     Describes a linear relationship between control- and target signals.
     """
-    def __init__(self, control_unit: str, target_unit: str, prop: float):
+    def __init__(self, prop: float, control_unit: str = None, target_unit: str = None) -> None:
         """
-        Prop: constant of proportionality.
+        Prop: constant of proportionality, i.e. target = control * prop
         """
         self._prop = prop
         super().__init__(control_unit, target_unit)
     
-    def convert(self, control_value):
+    def to_target(self, control_value: float) -> float:
         return control_value * self._prop
+
+    def to_control(self, target_value: float) -> float:
+        return target_value / self._prop
 
 # TODO: IMPLEMENT INTERPOLATION-BASED CALIBRATION
---
+++ interfaces/output.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from calibration import Calibration
+
+class Output(ABC):
+    """
+    Abstract base class for an output device. A derived class is to be made for every type of output,
+    and the connection details should be covered in the enter/exit methods.
+    Writing values should be internally implemented in the '_write' method, and invoked using the
+    'target' property. By default, reading the 'target' property returns the last set target value. Change this
+    behaviour to implement feedback by editing the 'target' setter and changing 'has_feedback' to return
+    true.
+    """
+
+    def __init__(self, default: float = None, calibration: Calibration = None) -> None:
+        self._last_set = None       # Last set value of TARGET signal
+        self._calibration = Calibration.standard() if calibration is None else calibration
+        # TODO: Implement default values
+
+    # __enter__ and __exit__ are only defined to allow subclasses to override them.
+    @abstractmethod
+    def __enter__(self) -> Output:
+        """
+        Open connection with device.
+        """
+        pass
+    
+    @abstractmethod
+    def __exit__(self, *_) -> None:
+        """
+        Close connections with device.
+        """
+        pass
+
+    @property
+    def target(self) -> float:
+        """
+        Getter for the 'value' property. Returns last set target value, by default
+        """
+        return self._last_set
+
+    @target.setter
+    def target(self, value: float) -> None:
+        """
+        Set a target value for the signal.
+        """
+        self._write(self._calibration.to_control(value))
+        self._last_set = value
+
+    @property
+    def control(self) -> float:
+        return self._calibration.to_control(self._last_set)
+
+    @property
+    def has_feedback() -> bool:
+        return False
+    
+    @abstractmethod
+    def _write(self, control_signal: float) -> None:
+        """
+        Actual implementation of writing to device
+        """
+        pass
