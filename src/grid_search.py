83883fee6f778c23a6fa1ce4a783fc2c491edcd7
Gustav Lidberg <gustav98.lidberg@gmail.com>
2022-11-23 13:37:55 +0100

    Fixed grid search a bit

--- src/grid_search.py
+++ src/grid_search.py
@@ -4,22 +4,63 @@ from time import sleep
 
 def grid_search(*args: tuple[Output, list[int], float]) -> None:
     """
-    Every argument is a tuple of the type (OutputObj, ValuesToCheck, WaitTime)
+    This is an example of a worker function, that defines something tedious that should be done over
+    some period of time. Such a function has to be run using an Action component. Also, any updates to the
+    GUI from this function must be handled via the GUIUpdater.update method, see below.
+
+    This particular function conducts a grid search over some Output devices. For every device to include in the
+    grid search, pass a tuple to the function as an argument. Every such tuple should be of the form:
+        (OutputObj, ValuesToCheck, WaitTime)
+    where:
+        OutputObj is a reference to the Output component instance,
+        ValuesToCheck is a (preferrably ordered) list of output values to check with this output, and
+        WaitTime is the minimum time (in seconds) of waiting that is required for the output device to "settle"
+            after changing the value, before measuring or similar.
+
+    The internal variable 'inverted' is used to keep track of whether the outputs were last set to the first value
+        in their value list (inverted[i] = False) or the last value in their value list (inverted[i] = True). This
+        allows iterating over the values in either forward- or backwards direction, eliminating the need for "jumps"
+        back to the initial value.
     """
-    print(args)
-    _grid_search(*args, min_wait_time=0)
+    inverted = [False for _ in args]
+    _initialize(*args, min_wait_time=0)
+    _grid_search(*args, inverted=inverted)
 
 
-def _grid_search(*args: tuple[Output, list[int], float], min_wait_time: float) -> None:
+def _grid_search(*args: tuple[Output, list[int], float], inverted: list[bool]) -> list[bool]:
+    """
+    This is a local and recursice implementation of the grid search algorithm.
+    It will only be called with at least one device in args. Thanks to the inversion
+    optimization, all other devices can always be assumed to be in the right position.
+    Whenever an Output changes its value, it waits the given time and then initializes
+    a measurement. Additionally, the last output in the call chain will activate one measurement
+    the first time it is called.
+    """
+    output, values, wait_time = args[0]
+    first = True
+    res = inverted[1:]
+    for val in (reversed(values) if inverted[0] else values):
+        if not first:
+            GUIUpdater.update(output, "set_value", val, True)
+            print(f'Sleeping {wait_time}')
+            sleep(wait_time)
+            print('Measured')
+        else:
+            if len(args) == 1:
+                print('Measured')
+            first = False
+        if len(args) > 1:
+            res = _grid_search(*args[1:], inverted=res)
+
+    inverted = [not inverted[0], *res]
+    return inverted
+
+
+def _initialize(*args: tuple[Output, list[int], float], min_wait_time: float) -> list[bool]:
     if not args:
-        # Time to measure!
         sleep(min_wait_time)
-        print('Measured!')
     else:
         output, values, wait_time = args[0]
-        print(f'Looping over {output.name}')
-        for val in values:
-            print(f'Setting {output.name} to {val}')
-            GUIUpdater.update(output, "set_value", val, True)
-            _grid_search(*args[1:], min_wait_time=max(min_wait_time, wait_time))
-            min_wait_time = 0
+        assert values  # Must be at least one value per Output
+        GUIUpdater.update(output, "set_value", values[0], True)
+        _initialize(*args[1:], min_wait_time=max(min_wait_time, wait_time))
--- src/main.py
+++ src/main.py
@@ -6,70 +6,27 @@ from pythion import Output, Input, MainWindow, PlotStream, Action
 
 from grid_search import grid_search
 
-PICO = False
-RS = False
-
-
-# Define invalid input handler
-def on_invalid() -> None:
-    print('Invalid!')
-
-
-# Define hardware intefaces:
-# RS 3005P
-MAX_VOLTAGE_RS = 5
-MAX_CURRENT_RS = 50
-MODE_RS = RS3000Output.PowerOptions.CURRENT
-INPUT_MAX_RS = MAX_VOLTAGE_RS if MODE_RS == RS3000Output.PowerOptions.VOLTAGE else MAX_CURRENT_RS
-CALIBRATION_RS = InterpolCalibration.from_file("testcal.csv", True)
-
-rs: OutputInterface
-pico: OutputInterface
-if RS:
-    port_rs = PortSelector.get_port_of('rs')
-    assert port_rs is not None
-    rs = RS3000Output(port=port_rs,
-                      calibration=CALIBRATION_RS,
-                      voltage_limit=MAX_VOLTAGE_RS,
-                      current_limit=MAX_CURRENT_RS,
-                      mode=MODE_RS)
-else:
-    rs = MockOutput()
-
-# Pico
-MAX_VOLTAGE_PICO = 350
-CALIBRATION_PICO = LinearCalibration(350)
-if PICO:
-    port_pico = PortSelector.get_port_of('pico')
-    assert port_pico is not None
-    pico = PicoOutput(port=port_pico, calibration=CALIBRATION_PICO, voltage_limit=MAX_VOLTAGE_PICO)
-else:
-    pico = MockOutput()
-
-# Input
-# port_rbd = PortSelector.get_port_of('rbd')
-# inp = RBDInput(
-# port=port_rbd,
-# rbd_sample_rate=5,
-# pull_rate=5,
-# unit=RBDInput.CurrentUnit.NANO
-# )
-inp = MockInput()
+op1 = MockOutput()
+op2 = MockOutput()
+op3 = MockOutput()
 
 # Setup GUI
+inp = MockInput()
 
-with rs, pico, inp:
-    inp.start_sampling(5)
+with op1, op2, op3, inp:
+    inp.start_sampling(20)
     win = MainWindow(high_resolution=False)
-    pico_component = Output(max_value=400, interface=pico, parent=win.main_widget(), name="High voltage supply", unit="V")
-    rs_component = Output(max_value=INPUT_MAX_RS, interface=rs, parent=win, name="Magnet current", unit="mA")
+    com1 = Output(max_value=400, interface=op1, parent=win.main_widget(), name="High voltage supply", unit="V")
+    com2 = Output(max_value=400, interface=op2, parent=win.main_widget(), name="High voltage supply", unit="V")
+    com3 = Output(max_value=400, interface=op3, parent=win.main_widget(), name="High voltage supply", unit="V")
 
     grid_search_args = [
-        (pico_component, [0, 100, 200, 300], 2),
-        (rs_component, [0, 5, 10, 15], 1)
+        (com1, [100, 200, 300], 2),
+        (com2, [100, 200, 300], 3),
+        (com3, [100, 200, 300], 1)
     ]
     action = Action(grid_search, *grid_search_args, parent=win.main_widget(), text='Tryck på mig!')
-    input_component = Input(interface=inp, name='Beam current', unit='nA', parent=win.main_widget())
+    # input_component = Input(interface=inp, name='Beam current', unit='nA', parent=win.main_widget())
     plt = PlotStream(parent=win.main_widget(), input=inp, timespan=10, fix_scale=False)
-    win.add_children(pico_component, rs_component, action, input_component, plt)
+    win.add_children(com1, com2, com3, action, plt)
     win.run()
