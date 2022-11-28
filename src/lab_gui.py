3431a6ae2b77f1483542cbb38fd77b178940dec5
Gustav Lidberg <gustav98.lidberg@gmail.com>
2022-11-28 14:41:03 +0100

    Lab gui changes

--- src/lab_gui.py
+++ src/lab_gui.py
@@ -1,5 +1,5 @@
 # Hardware interface classes
-from pythion.connections import PortSelector, RBDInput, RS3000Output
+from pythion.connections import PortSelector, RBDInput, RS3000Output, PicoOutput, LinearCalibration
 
 # GUI Classes
 from pythion import Output, Input, MainWindow, PlotStream
@@ -10,10 +10,19 @@ assert port_magnet
 magnet_output_device = RS3000Output(
     port=port_magnet,
     voltage_limit=5,
-    current_limit=30,
+    current_limit=1000,
     mode=RS3000Output.PowerOptions.CURRENT
 )
 
+# Velocity filter
+port_velocity = PortSelector.get_port_of('pico')
+cal = LinearCalibration(350)
+assert port_velocity
+velocity_output_device = PicoOutput(
+    port=port_velocity,
+    calibration=cal,
+    voltage_limit=300
+)
 # Input
 port_rbd = PortSelector.get_port_of('rbd')
 assert port_rbd
@@ -25,15 +34,22 @@ input_device = RBDInput(
 )
 
 # Setup GUI
-with input_device, magnet_output_device:
+with input_device, magnet_output_device, velocity_output_device:
     input_device.start_sampling(5)
     win = MainWindow(high_resolution=False)
-    output_component = Output(
+    magnet_component = Output(
         max_value=30,
         interface=magnet_output_device,
         parent=win.main_widget(),
         name="Magnet",
         unit="mA"
+    )
+    velocity_filter_component = Output(
+        max_value=300,
+        interface=velocity_output_device,
+        parent=win.main_widget(),
+        name="Velocity Filter",
+        unit="V"
     )
     input_component = Input(
         interface=input_device,
@@ -47,5 +63,5 @@ with input_device, magnet_output_device:
         timespan=10,
         fix_scale=False
     )
-    win.add_children(output_component, input_component, plt_component)
+    win.add_children(magnet_component, velocity_filter_component, input_component, plt_component)
     win.run()
