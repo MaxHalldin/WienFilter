1a0383e48086c76565777955afb3b60fc884e853
Gustav Lidberg <gustav98.lidberg@gmail.com>
2022-11-29 11:01:27 +0100

    Some changes to the lab gui

--- src/lab_gui.py
+++ src/lab_gui.py
@@ -4,13 +4,32 @@ import numpy as np
 # GUI Classes
 from pythion import Output, Input, MainWindow, PlotStream, GridSearch
 
+
+# Lab GUI parameters, to be read from config file!
+#   Device setting params
+MAGNETCONTROL_MAXCURRENT = 3000  # mA
+VELOCITYCONTROL_MAXVOLTAGE = 300  # V
+CURRENT_SAMPLERATE = 10  # Hz
+CURRENT_PULLRATE = 5  # Hz
+CURRENTGRAPH_REFRESHRATE = 5  # Hz
+#   Grid search params
+MAGNET_SETTIME = 1.5  # s
+VELOCITY_SETTIME = 1.5  # s
+MEASURING_TIME = 0.5  # s
+MAGNET_STARTVALUE = 0  # mA
+MAGNET_STEPSIZE = 100  # mA
+MAGNET_ENDVALUE = 3000  # mA
+VELOCITY_STARTVALUE = 0  # mA
+VELOCITY_STEPSIZE = 10  # mA
+VELOCITY_ENDVALUE = 300  # mA
+
 # Magnet output
 port_magnet = PortSelector.get_port_of('rs')
 assert port_magnet
 magnet_output_device = RS3000Output(
     port=port_magnet,
-    voltage_limit=5,
-    current_limit=1000,
+    voltage_limit=30,
+    current_limit=3000,
     mode=RS3000Output.PowerOptions.CURRENT
 )
 
@@ -28,24 +47,24 @@ port_rbd = PortSelector.get_port_of('rbd')
 assert port_rbd
 input_device = RBDInput(
     port=port_rbd,
-    rbd_sample_rate=5,
-    pull_rate=5,
+    rbd_sample_rate=CURRENT_SAMPLERATE,
+    pull_rate=CURRENT_PULLRATE,
     unit=RBDInput.CurrentUnit.NANO
 )
 
 # Setup GUI
 with input_device, magnet_output_device, velocity_output_device:
-    input_device.start_sampling(5)
+    input_device.start_sampling(CURRENTGRAPH_REFRESHRATE)
     win = MainWindow(high_resolution=False)
     magnet_component = Output(
-        max_value=30,
+        max_value=MAGNETCONTROL_MAXCURRENT,
         interface=magnet_output_device,
         parent=win.main_widget(),
         name="Magnet",
         unit="mA"
     )
     velocity_filter_component = Output(
-        max_value=300,
+        max_value=VELOCITYCONTROL_MAXVOLTAGE,
         interface=velocity_output_device,
         parent=win.main_widget(),
         name="Velocity Filter",
@@ -57,10 +76,15 @@ with input_device, magnet_output_device, velocity_output_device:
         unit='nA',
         parent=win.main_widget()
     )
-    gs = GridSearch(input=input_device, measuring_time=1, parent=win.main_widget(), move_knobs=True, measurement_str='Battery Current')
-    volt_values = [round(x) for x in np.arange(0, 301, 30)]
-    magnet_values = [round(x) for x in np.arange(0, 1001, 100)]
+    gs = GridSearch(input=input_device,
+                    measuring_time=MEASURING_TIME,
+                    parent=win.main_widget(),
+                    move_knobs=True,
+                    measurement_str='Battery Current')
+    volt_values = [round(x) for x in np.arange(VELOCITY_STARTVALUE, VELOCITY_ENDVALUE+VELOCITY_STEPSIZE/2, VELOCITY_STEPSIZE)]
+    magnet_values = [round(x) for x in np.arange(MAGNET_STARTVALUE, MAGNET_ENDVALUE+MAGNET_STEPSIZE/2, MAGNET_STEPSIZE)]
     gs.add_device(magnet_component, magnet_values, 1)
     gs.add_device(velocity_filter_component, volt_values, 1)
-    win.add_children(magnet_component, velocity_filter_component, input_component, gs)
+    plot = PlotStream(parent=win.main_widget(), input=input_device, timespan=30)
+    win.add_children(magnet_component, velocity_filter_component, input_component, gs, plot)
     win.run()
